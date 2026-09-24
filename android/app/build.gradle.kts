143f3912297c32ebbaad84809c141bfcff794e8a
Ayesh Chamodye <ayeshchamodye@gmail.com>
2026-09-24 14:49:59 +0530

    build(android): upgrade compileSdk and targetSdk to 37
    Update Android build configuration to use compileSdk 37 and targetSdk 37.
    Also specify a fixed NDK version (28.2.13676358) and update the release
    build information and artifacts.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,8 +6,8 @@ plugins {
 
 android {
     namespace = "com.ayesh.dev.qrtoolkit.qrtoolkit"
-    compileSdk = 36
-    ndkVersion = flutter.ndkVersion
+    compileSdk = 37
+    ndkVersion = "28.2.13676358"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
@@ -15,12 +15,9 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.ayesh.dev.qrtoolkit.qrtoolkit"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
-        targetSdk = 35
+        targetSdk = 37
         // Uses the version code from pubspec.yaml. When using split APKs, 1000 * ABI_VERSION
         // is added automatically by Flutter. (https://developer.android.com/studio/build/configure-apk-splits#configure-APK-versions)
         // You can force using the value of versionCode by specifying the `-P force-version-code-ignoring-abi=true`
